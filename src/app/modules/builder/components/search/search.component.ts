0ca05b7d42dfa0293097ac560f07388fe9e3df1c
w-raken <maxime.marquet1@gmail.com>
2019-06-12 15:13:28 +0200

    validation image name

--- src/app/modules/builder/components/search/search.component.ts
+++ src/app/modules/builder/components/search/search.component.ts
@@ -10,8 +10,9 @@ export class SearchComponent implements OnInit {
 
   @Output() next = new EventEmitter();
 
-  private apiSearch: string = null;
-  private resultSearch: null;
+  public apiSearch: string = null;
+  public resultSearch: object = null;
+  public customSearch: boolean = false;
 
   constructor(private api: ApiService) { }
 
@@ -20,12 +21,23 @@ export class SearchComponent implements OnInit {
 
   public searchApi() {
     this.api.get(`registry/search?query=${encodeURIComponent(this.apiSearch)}`).toPromise().then(res => {
-      this.resultSearch = res;
+      if (res) {
+        this.customSearch = false;
+        this.resultSearch = res;
+        return;
+      }
+
+      this.resultSearch = [{
+        name: this.apiSearch,
+        star_count: 0,
+        pull_count: 0
+      }];
+      this.customSearch = true;
     });
   }
 
   public handleNext(imageName): void {
-    if (imageName.split('/').length - 1 < 2) {
+    if (imageName.split('/').length - 1 <= 2) {
       Object.getOwnPropertyNames(this.resultSearch).map(key => {
         if (this.resultSearch[key].name === imageName) {
           this.next.emit(this.resultSearch[key].name);
