03f7006aa67dee664d7ffc62e3716afad36b1428
w-raken <maxime.marquet1@gmail.com>
2019-06-13 10:29:53 +0200

    WIP tags config

--- src/app/modules/builder/components/config/config.component.ts
+++ src/app/modules/builder/components/config/config.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ApiService} from '../../../../core/services/api.service';
 
 @Component({
   selector: 'app-builder-config',
@@ -8,9 +9,23 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ConfigComponent implements OnInit {
 
   @Input() imageName: string;
+  public tags = null;
+  public tagSearch: string = null;
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.api.get(`registry/tag?query=${encodeURIComponent(this.imageName)}`).toPromise().then(res => {
+      this.tags = JSON.parse(res).results;
+      console.log(this.tags.length);
+    });
+  }
+
+  public searchTag(): void {
+    Object.getOwnPropertyNames(this.tags).map(key => {
+      if (this.tags[key].name === this.tagSearch) {
+        // console.log(this.tags[key]);
+      }
+    });
   }
 }
