45963f571f620d0ea3a36d71871765d831e454ff
Laurent Bassin <laurent@bassin.info>
2019-06-19 22:01:44 +0200

    Display overlay during group change

--- src/app/modules/builder/components/form/form.component.ts
+++ src/app/modules/builder/components/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { ContainerConfigGroup } from '../../models/container-config-group';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt';
+import { faEraser } from '@fortawesome/free-solid-svg-icons/faEraser';
 
 @Component({
   selector: 'app-builder-form',
@@ -10,12 +12,28 @@ export class FormComponent {
 
   public groupsData: Array<ContainerConfigGroup>;
 
+  public editIcon;
+  public deleteIcon;
+
+  public isEditing: boolean;
+  public isRemoving: boolean;
+
+  public overlayVisible: boolean;
+
   @Output() addAction = new EventEmitter();
   @Output() groupsChange = new EventEmitter<ContainerConfigGroup[]>();
 
   @Input() set groups(data: ContainerConfigGroup[]) {
     this.groupsData = data;
     this.groupsChange.emit(data);
+
+    this.editIcon = faPencilAlt;
+    this.deleteIcon = faEraser;
+
+    this.isEditing = false;
+    this.isRemoving = false;
+
+    this.overlayVisible = false;
   }
 
   remove(group: ContainerConfigGroup): void {
@@ -25,5 +43,19 @@ export class FormComponent {
 
   openPopIn(group: ContainerConfigGroup): void {
     this.addAction.emit(group);
+  }
+
+  handleEdit(): void {
+    this.toggleOverlay();
+    this.isEditing = true;
+  }
+
+  handleRemove(): void {
+    this.toggleOverlay();
+    this.isRemoving = true;
+  }
+
+  private toggleOverlay(): void {
+    this.overlayVisible = !this.overlayVisible;
   }
 }
