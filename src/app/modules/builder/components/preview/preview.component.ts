8ed73fb8b071b667b439e40df5cc3887424791e0
Laurent Bassin <laurent@bassin.info>
2019-06-30 09:28:33 +0200

    Display error on preview

--- src/app/modules/builder/components/preview/preview.component.ts
+++ src/app/modules/builder/components/preview/preview.component.ts
@@ -1,64 +1,63 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { ContainerConfigGroup } from '../../models/container-config-group';
 import { ApiService } from '../../../../core/services/api.service';
 import { throwError } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { ContainerService } from '../../services/container.service';
-import { Container } from '../../models/container';
 
 @Component({
-  selector: 'app-builder-preview',
-  templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+    selector: 'app-builder-preview',
+    templateUrl: './preview.component.html',
+    styleUrls: ['./preview.component.scss']
 })
 export class PreviewComponent {
 
-  preview = '';
-  loading = true;
-  faSpinner = faSpinner;
+    preview = '';
+    loading = true;
+    faSpinner = faSpinner;
 
-  constructor(private api: ApiService, private containerService: ContainerService) {
-  }
-
-  @Input() image: string;
-
-  @Input() set config(config) {
-    const data = {
-      id: '',
-      image: this.image,
-      config: {}, // Empty
-      form: {
-        id: this.image,
-        name: this.image,
-        config
-      }
-    };
-
-    const apiArg = this.containerService.formDataToApiArg(data);
-    this.updatePreview(apiArg);
-  }
-
-  private updatePreview(apiArg: any): void {
-    this.api.post('preview', {...apiArg, builder: true})
-      .pipe(
-        map((response: { content: string }) => response.content),
-        catchError((error: HttpErrorResponse) => throwError(error.message))
-      )
-      .toPromise()
-      .then((response) => this.setPreview(response));
-  }
-
-  private setPreview(data: string) {
-    if (data === undefined) {
-      this.preview = '# Configuration is not valid';
-      this.loading = false;
-
-      return;
+    constructor(private api: ApiService, private containerService: ContainerService) {
     }
 
-    this.loading = !data;
-    this.preview = YAML.stringify(data, 8);
-  }
+    @Input() image: string;
+
+    @Input() set config(config) {
+        const data = {
+            id: '',
+            image: this.image,
+            config: {}, // Empty
+            form: {
+                id: this.image,
+                name: this.image,
+                config
+            }
+        };
+
+        const apiArg = this.containerService.formDataToApiArg(data);
+        this.updatePreview(apiArg);
+    }
+
+    private updatePreview(apiArg: any): void {
+        this.api.post('preview', {...apiArg, builder: true})
+            .pipe(
+                map((response: { content: string }) => response.content),
+                catchError((error: HttpErrorResponse) => throwError(error.message))
+            )
+            .toPromise()
+            .then((response) => this.setPreview(response))
+            .catch(() => this.setPreview(undefined));
+    }
+
+    private setPreview(data: string) {
+        if (data === undefined) {
+            this.preview = '# Configuration is not valid';
+            this.loading = false;
+
+            return;
+        }
+
+        this.loading = !data;
+        this.preview = YAML.stringify(data, 8);
+    }
 }
